4a807f7313a4a66bb9258043f20eba4a2d6b5f48
AndreeaMatei2811 <alm.matei@gmail.com>
2020-10-12 09:27:41 +0200

    schimbare

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Buna seara!!!
+          Buna dimineata!!!
         </p>
       </header>
     </div>
