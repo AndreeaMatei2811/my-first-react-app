156283fb28aae825911c7a474432efc18195b504
AndreeaMatei2811 <alm.matei@gmail.com>
2020-10-12 19:24:13 +0200

    Pokemon exercise

--- src/App.js
+++ src/App.js
@@ -1,30 +1,72 @@
-import React from 'react';
+import React from "react";
+import Pokemon from "./components/Pokemon/Pokemon";
+import Title from "./components/Title";
 
-function Hello (){
-  return <h1> say hello!</h1>
-}
+// function Hello() {
+//   return <h1> say hello!</h1>;
+// }
 
-function Greeting ({age, name}){
-  const message = age
-  ? `you are ${age}`
-   : `I do not know how old are you`
-  return <div>
-  < Hello/>
-  <p>this is your name: {name}!</p>
-  <p>and this is your age: {message}</p>
-  </div>
-}
-
-
+// function Greeting({ age, name }) {
+//   const message = age ? `you are ${age}` : `I do not know how old are you`;
+//   return (
+//     <div>
+//       <Hello />
+//       <p>this is your name: {name}!</p>
+//       <p>and this is your age: {message}</p>
+//     </div>
+//   );
+// }
 
 function App() {
+  const allPokemon = [
+    {
+      name: "Charizard",
+      weight: 90,
+      awesome: true,
+      terrifying: false,
+      abilities: ["Blaze", "Solar power", "Tough claws", "Drought"],
+    },
+    {
+      name: "Bulbasaur",
+      weight: 6.9,
+      awesome: false,
+      terrifying: false,
+      abilities: ["Overgrow", "Chlorophyll"],
+    },
+    {
+      name: "Mewtwo",
+      weight: 122,
+      awesome: true,
+      terrifying: true,
+      abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"],
+    },
+    {
+      name: "Mega beedrill",
+      weight: 65,
+      awesome: false,
+      terrifying: true,
+      abilities: ["Intimidate", "Unnerve"],
+    },
+  ];
+
   return (
-      <div> 
-      < Greeting name= "david"  age={30}/>
-      < Greeting name="andreea"/>
-      < Greeting name= "mika"  age={4}/>
+    <main className="container">
+      <Title content="Some Simple Title" />
+      <div className="row">
+        {allPokemon.map((pokemon) => (
+          <div className="col-md-6 col-lg-4">
+            <Pokemon
+              name={pokemon.name}
+              weight={pokemon.weight}
+              awesome={pokemon.awesome}
+              terrifying={pokemon.terrifying}
+              abilities={pokemon.abilities}
+            />
+          </div>
+        ))}
       </div>
-  )
+    </main>
+  );
 }
 
 export default App;
---
+++ src/test.js
@@ -0,0 +1,17 @@
+// const res = ["Andreea", "Maria", "Mirabela", "Alina"].map((name) => {
+//   return name.length;
+// });
+
+// const allPokemon = [
+//   { name: "Charizard", weight: 90 },
+//   { name: "Bulbasaur", weight: 6.9 },
+//   { name: "Mewtwo", weight: 122 },
+//   { name: "Mega beedrill", weight: 65 },
+// ];
+
+// const res = allPokemon.map((pokemon) => {
+//   const { name, weight } = pokemon;
+//   return `${name}: ${weight} kg`;
+// });
+
+// console.log(res);
